6b4c83f4917a8a5181e919aaaec0158d52952db8
Victor Aremu <me@victorare.mu>
2024-02-03 23:19:42 +0100

    feat: don't fallback to lsp formatting

--- dot_config/nvim/lua/plugins/lsp_formatting.lua
+++ dot_config/nvim/lua/plugins/lsp_formatting.lua
@@ -24,7 +24,7 @@ return {
 					["end"] = { args.line2, end_line:len() },
 				}
 			end
-			require("conform").format({ async = true, lsp_fallback = true, range = range })
+			require("conform").format({ async = true, lsp_fallback = false, range = range })
 		end, { range = true })
 
 		return {
