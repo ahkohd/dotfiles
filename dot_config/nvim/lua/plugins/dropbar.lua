18330721f826ec19d0015c57aacffda07245c068
Victor Aremu <me@victorare.mu>
2024-02-09 02:03:09 +0100

    chore: update desc message

--- dot_config/nvim/lua/plugins/dropbar.lua
+++ dot_config/nvim/lua/plugins/dropbar.lua
@@ -1,7 +1,7 @@
 return {
 	"Bekaboo/dropbar.nvim",
 	keys = {
-		{ "n", "<leader>u", "<cmd>lua require('dropbar.api).pick()<cr>" },
+		{ "<leader>u", "<cmd>lua require('dropbar.api).pick()<cr>", desc = "Enter dropbar pick mode" },
 	},
 	dependencies = {
 		"nvim-telescope/telescope-fzf-native.nvim",
