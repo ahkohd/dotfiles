10064a98252e08ef71bcf97ae41776f36aa6579c
Victor Aremu <me@victorare.mu>
2024-02-09 01:57:00 +0100

    feat: <leader>u picks dropbar

--- dot_config/nvim/lua/plugins/dropbar.lua
+++ dot_config/nvim/lua/plugins/dropbar.lua
@@ -1,5 +1,8 @@
 return {
 	"Bekaboo/dropbar.nvim",
+	keys = {
+		{ "n", "<leader>u", "<cmd>lua require('dropbar.api)<cr>" },
+	},
 	dependencies = {
 		"nvim-telescope/telescope-fzf-native.nvim",
 	},
