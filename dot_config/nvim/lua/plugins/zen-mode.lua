123451ecf8dd3187c975d504e2e49f206ed6390c
Victor Aremu <me@victorare.mu>
2024-02-09 13:17:55 +0100

    chore: improve config

--- dot_config/nvim/lua/plugins/zen-mode.lua
+++ dot_config/nvim/lua/plugins/zen-mode.lua
@@ -2,7 +2,16 @@ return {
 	"folke/zen-mode.nvim",
 	event = "BufRead",
 	opts = {
+		options = {
+			number = false,
+			signcolumn = "no",
+			cursorline = true,
+		},
 		plugins = {
+			options = {
+				enabled = true,
+				laststatus = 0,
+			},
 			alacritty = {
 				enabled = true,
 				font = 14,
