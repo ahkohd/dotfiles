d7ea04c6ca802cdc5dfe5b889e6539e8baf10d40
Victor Aremu <me@victorare.mu>
2024-02-09 02:07:10 +0100

    fix: space is my leader

--- dot_config/nvim/lua/plugins/dropbar.lua
+++ dot_config/nvim/lua/plugins/dropbar.lua
@@ -2,7 +2,7 @@ return {
 	"Bekaboo/dropbar.nvim",
 	event = "BufRead",
 	keys = {
-		{ "<leader>u", "<cmd>lua require('dropbar.api).pick()<cr>", desc = "Enter dropbar pick mode" },
+		{ "<space>u", "<cmd>lua require('dropbar.api).pick()<cr>", desc = "Enter dropbar pick mode" },
 	},
 	dependencies = {
 		"nvim-telescope/telescope-fzf-native.nvim",
