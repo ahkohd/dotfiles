7a35017aaa17d9590e9634dbcd6075ed1b195dca
Victor Aremu <me@victorare.mu>
2024-02-11 19:23:17 +0100

    feat: add chatgpt nvim

---
+++ dot_config/nvim/lua/plugins/chat-gpt.lua
@@ -0,0 +1,15 @@
+return {
+	"jackMort/ChatGPT.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"folke/trouble.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		require("chatgpt").setup({
+			api_key_cmd = "op read op://Personal/OpenAiApi/credential --no-newline",
+		})
+	end,
+}
