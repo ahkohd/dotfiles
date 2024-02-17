d18bce5fc028bd49b491f4d125cd1d5f7a694f7a
Victor Aremu <me@victorare.mu>
2024-02-17 13:02:56 +0100

    feat: add pencil shortcuts

--- dot_config/nvim/lua/plugins/pencil.lua
+++ dot_config/nvim/lua/plugins/pencil.lua
@@ -1,5 +1,30 @@
+-- luacheck: globals vim
+
 return {
 	"preservim/vim-pencil",
 	event = "VeryLazy",
+	keys = {
+		{
+			"<space>l",
+			description = "Pencil",
+			function()
+				vim.cmd("PencilToggle")
+			end,
+		},
+		{
+			"<space>ls",
+			description = "PencilSoft",
+			function()
+				vim.cmd("PencilSoft")
+			end,
+		},
+		{
+			"<space>lh",
+			description = "PencilHard",
+			function()
+				vim.cmd("PencilHard")
+			end,
+		},
+	},
 	config = function() end,
 }
