e8c07a7ab7732b74d30fea0a76d581939e75ff9a
Victor Aremu <me@victorare.mu>
2024-02-19 20:24:08 +0100

    refactor: use op

--- dot_config/nvim/lua/plugins/chat-gpt.lua
+++ dot_config/nvim/lua/plugins/chat-gpt.lua
@@ -10,10 +10,8 @@ return {
 		"nvim-telescope/telescope.nvim",
 	},
 	config = function()
-		local home = vim.fn.expand("$HOME")
-
 		require("chatgpt").setup({
-			api_key_cmd = "gpg --decrypt " .. home .. "/openai.gpg",
+			api_key_cmd = "op read op://Personal/OpenAiApi/credential --no-newline",
 		})
 	end,
 }
