0726d00b41f23333e23e8f7b39fbfb71223fffb9
Victor Aremu <me@victorare.mu>
2024-02-21 05:56:03 +0100

    chore: update

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -18,39 +18,6 @@ return {
 		local cmp = require("cmp")
 		local lspkind = require("lspkind")
 
-		local lspkind_comparator = function(conf)
-			local lsp_types = require("cmp.types").lsp
-			return function(entry1, entry2)
-				-- Prioritize 'copilot' over everything else
-				if entry1.source.name == "copilot" then
-					return nil
-				elseif entry2.source.name == "copilot" then
-					return false
-				end
-
-				if entry1.source.name ~= "nvim_lsp" then
-					if entry2.source.name == "nvim_lsp" then
-						return false
-					else
-						return nil
-					end
-				end
-				local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
-				local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
-
-				local priority1 = conf.kind_priority[kind1] or 0
-				local priority2 = conf.kind_priority[kind2] or 0
-				if priority1 == priority2 then
-					return nil
-				end
-				return priority2 < priority1
-			end
-		end
-
-		local label_comparator = function(entry1, entry2)
-			return entry1.completion_item.label < entry2.completion_item.label
-		end
-
 		local has_words_before = function()
 			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
 				return false
@@ -64,40 +31,6 @@ return {
 				expand = function(args)
 					require("luasnip").lsp_expand(args.body)
 				end,
-			},
-			sorting = {
-				comparators = {
-					lspkind_comparator({
-						kind_priority = {
-							Field = 11,
-							Property = 11,
-							Constant = 10,
-							Enum = 10,
-							EnumMember = 10,
-							Event = 10,
-							Function = 10,
-							Method = 10,
-							Operator = 10,
-							Reference = 10,
-							Struct = 10,
-							Variable = 9,
-							File = 8,
-							Folder = 8,
-							Class = 5,
-							Color = 5,
-							Module = 5,
-							Keyword = 2,
-							Constructor = 1,
-							Interface = 1,
-							Snippet = 0,
-							Text = 1,
-							TypeParameter = 1,
-							Unit = 1,
-							Value = 1,
-						},
-					}),
-					label_comparator,
-				},
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
@@ -117,13 +50,11 @@ return {
 				end),
 			}),
 			sources = cmp.config.sources({
-				{ name = "copilot" },
 				{ name = "nvim_lsp" },
-			}, {
 				{ name = "luasnip" },
 				{ name = "path" },
-			}, {
 				{ name = "buffer" },
+				{ name = "copilot" },
 			}),
 			formatting = {
 				format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
