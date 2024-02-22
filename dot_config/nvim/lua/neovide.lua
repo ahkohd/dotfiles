95795fe3af205804bc1ebd2906ea4ed799fc2d37
Victor Aremu <me@victorare.mu>
2024-02-22 14:26:25 +0100

    update

--- dot_config/nvim/lua/neovide.lua
+++ dot_config/nvim/lua/neovide.lua
@@ -1,4 +1,4 @@
-vim.o.guifont = "BerkeleyMono Nerd Font Plus Font Awesome Plus Octicons Plus Codicons Plus Material Design Icons:h15"
+vim.o.guifont = "BerkeleyMono Nerd Font Plus Font Awesome Plus Octicons Plus Codicons Plus Material Design Icons:h18"
 
 vim.g.neovide_refresh_rate = 120
 
@@ -7,3 +7,5 @@ vim.g.neovide_confirm_quit = false
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
 vim.g.neovide_window_blurred = true
+
+vim.g.neovide_transparency = 0.0
