ef8850d69686e04241067d3fedcf1be0da4685a7
Victor Aremu <me@victorare.mu>
2025-08-26 06:13:04 +0100

    feat: add yamb.yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -12,3 +12,35 @@ require("eza-preview"):setup({
 require("no-status"):setup()
 
 require("starship"):setup()
+
+local bookmarks = {}
+local path_sep = package.config:sub(1, 1)
+local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+
+if ya.target_family() == "windows" then
+  table.insert(bookmarks, {
+    tag = "Scoop Local",
+
+    path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
+    key = "p"
+  })
+  table.insert(bookmarks, {
+    tag = "Scoop Global",
+    path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
+    key = "P"
+  })
+end
+table.insert(bookmarks, {
+  tag = "Desktop",
+  path = home_path .. path_sep .. "Desktop" .. path_sep,
+  key = "d"
+})
+
+require("yamb"):setup {
+  bookmarks = bookmarks,
+  jump_notify = true,
+  cli = "fzf",
+  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
+        (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+}
