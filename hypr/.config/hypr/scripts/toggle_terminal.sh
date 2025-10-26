fa432bb5bf1a3e03472dd5da6dfd81e06c28591c
Victor Aremu <me@victorare.mu>
2025-10-26 17:13:56 +0000

    wip

--- hypr/.config/hypr/scripts/toggle_terminal.sh
+++ hypr/.config/hypr/scripts/toggle_terminal.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
-# if hyprctl clients | grep -i 'class: com.mitchellh.ghostty' | grep -v 'title: finder'; then
-#   hyprctl dispatch focuswindow 'class:^(com\.mitchellh\.ghostty)$'
-# else
-#   ghostty
-#   sleep 1.0
-#   hyprctl dispatch focuswindow 'class:^(com\.mitchellh\.ghostty)$'
-# fi
-
-if hyprctl clients | grep -i 'class: Alacritty'; then
-  hyprctl dispatch focuswindow 'class:^(Alacritty)$'
+if hyprctl clients | grep -i 'class: com.mitchellh.ghostty' | grep -v 'title: finder'; then
+  hyprctl dispatch focuswindow 'class:^(com\.mitchellh\.ghostty)$'
 else
-  alacritty
-  sleep 0.5
-  hyprctl dispatch focuswindow 'class:^(Alacritty)$'
+  ghostty
+  sleep 1.0
+  hyprctl dispatch focuswindow 'class:^(com\.mitchellh\.ghostty)$'
 fi
